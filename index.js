a8c7de991b9f933cf3cdb69042a408ccd1730084
Bjorkbat <bjorkbat@gmail.com>
2017-05-03 14:28:15 -0600

    added dotenv so I can hide database creds

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
+// Imports go up here
 const express = require('express');
 const exphbs = require('express-handlebars');
+const Sequelize = require('sequelize');
+
+// This import is a little different.  You're going to want to read the docs
+// at https://github.com/motdotla/dotenv
+// Basically, using this to hide certain sensitive things
+require('dotenv').config();
+
+// Init express
 const app = express();
 
+// Setup view engine
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+// Setup sequelize  We can get away with just using the simplest constructor
+// for now
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS
+);
 
 app.get('/', function(req, res) {
   res.render('generic', {title: "Home Page", message: "Hello World"});
