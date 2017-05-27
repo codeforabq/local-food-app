9dccfab336252e98196780a1397d6cef7534c1e1
Bjorkbat <bjorkbat@gmail.com>
2017-05-27 17:41:16 -0600

    you can create an account on this thing now

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Imports go up here
+const bcrypt = require('bcrypt-nodejs');
 const express = require('express');
 const exphbs = require('express-handlebars');
 const Sequelize = require('sequelize');
@@ -71,7 +72,40 @@ app.post('/signup', function(req, res) {
     return;
   }
 
+  if (req.body.username == "") {
+    res.render('auth', {title: "Create an Account", signup: true,
+      error: "No Username Provided"});
+    return;
+  }
+
+  if (req.body.email == "") {
+    res.render('auth', {title: "Create an Account", signup: true,
+      error: "No Email Provided"});
+    return;
+  }
+
   // Hash the password
+  bcrypt.hash(req.body.password, null, null, function(err, hash) {
+
+    // Save the user
+    models.User.create({
+      username: req.body.username,
+      email: req.body.email,
+      passhash: hash
+    }).then(function() {
+
+      res.render('auth', {title: "Login", signup: false, created: true});
+
+    }, function() {
+
+      // Couldn't save user, throw up a generic error
+      res.render('auth', {title: "Create an Account", signup: true,
+        error: "Couldn't create user at this time.  Try again later"});
+      return;
+
+    });
+
+  })
 
 });
 
