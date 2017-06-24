53b6a5f4098a6ac6fd4100850f7ba42e1971efbd
Bjorkbat <bjorkbat@gmail.com>
2017-06-24 16:35:47 -0600

    Added a vagrant file and provisioning script to make getting started easier

---
+++ provision.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Provisioning virtual machine..."
+apt-get update
+
+echo "Installing MySQL..."
+apt-get install debconf-utils -y > /dev/null
+debconf-set-selections <<< "mysql-server mysql-server/root_password password v3gg13s"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password v3gg13s"
+apt-get install mysql-server -y > /dev/null
+
+echo "Creating Database..."
+mysql --user="root" --password="v3gg13s" --execute="CREATE DATABASE localfood;"
+
+echo "Adding MySQL user..."
+mysql --user="root" --password="v3gg13s" --execute="CREATE USER 'localfooduser'@'localhost' IDENTIFIED BY 'v3gg13s';"
+mysql --user="root" --password="v3gg13s" --execute="GRANT ALL PRIVILEGES ON localfood.* TO 'localfooduser'@'localhost';";
+mysql --user="root" --password="v3gg13s" --execute="FLUSH PRIVILEGES;"
+
+echo "Installing Node v6.11.0..."
+curl -sL https://deb.nodesource.com/setup_6.x -o nodesource_setup.sh
+source nodesource_setup.sh
+apt-get install nodejs
