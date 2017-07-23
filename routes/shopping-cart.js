aad82d7a09436f7fe4cf90211e6f7cd30d8bd121
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-07-22 18:35:50 -0600

    added some variables to shopping cart decided on add, remove and check out (for now)

--- routes/shopping-cart.js
+++ routes/shopping-cart.js
@@ -1,8 +1,19 @@
 //shopping cart functions
 
+//once sessions is established be sure to link it here
+
 module.exports = {
 
-	getShoppingCart: function(add, remove, checkOut) {
+	//need to establish name will be the username and need to link with sessions
+	//so the shopping cart belongs to a real person
 
+	getShoppingCart: function(item) {
+		var add, remove, checkOut;
+},
+
+
+	add: function (price, quantity) {
+		var newItem = newShoppingCart
 	}
+
 }; //end module.exports object
